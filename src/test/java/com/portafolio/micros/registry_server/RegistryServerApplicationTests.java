9becb7483a7ff00aacc40b8dea2eb9c9c01151d0
AlexSanti33 <santiago.jav27@gmail.com>
2024-05-29 02:39:52 -0600

    first commit

---
+++ src/test/java/com/portafolio/micros/registry_server/RegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portafolio.micros.registry_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
